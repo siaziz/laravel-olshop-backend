166894df7ce326eedf5cfd2be77c3affb5ba5d98
Abdul Aziz <siaz1z@yahoo.com>
2024-03-07 17:30:11 +0700

    01-

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(9)->create();
+
+        $user  = \App\Models\User::factory()->create([
+            'name' => 'Admin Siaziz',
+            'email' => 'siaziz@gmail.com',
+            'password' => Hash::make('12345678'),
+            'phone' => '085719485566',
+            'roles' => 'ADMIN',
+        ]);
+
+    }
+}
